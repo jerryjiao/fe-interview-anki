b322e1b9d44f46d48e16eaeb97cfc0be38db3cc6
Jerry.jiao <jerry201382@gmail.com>
2019-11-10 09:47:00 +0800

    initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Vuepress',
+  description: 'This is a Zeit Now 2.0 example',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Config Page', link: '/config' },
+    ]
+  }
+}
