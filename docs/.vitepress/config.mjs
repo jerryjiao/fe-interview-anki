17bdc9d71ec5a6b48864583b4429535109850d73
jerryjiao <mailtojerry@foxmail.com>
2024-09-21 19:46:47 +0800

    feat: add real interview questions

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,14 +2,27 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "前端基础知识艾宾浩斯记忆（anki）",
-  description: "A VitePress Site",
+  title: "Frontend Basics Anki",
+  description: "A VitePress Site for Frontend Basics",
+  
+  locales: {
+    root: {
+      label: 'English',
+      lang: 'en'
+    },
+    zh: {
+      label: '中文',
+      lang: 'zh-CN',
+      link: '/zh/'
+    }
+  },
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: '前端基础知识', link: '/' },
-      { text: '面试真题', link: '/real' },
-      { text: '手写题', link: 'write'}
+      { text: 'Frontend Basics', link: '/' },
+      { text: 'Interview Questions', link: '/real' },
+      { text: 'Coding Questions', link: '/write' }
     ],
 
     socialLinks: [
