3ba4bc4936ad44535cb54d1c8e5cfff0c783ece4
jerryjiao <mailtojerry@foxmail.com>
2024-09-21 20:32:31 +0800

    feat: config

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/',
   title: "Frontend Basics Anki",
   description: "A VitePress Site for Frontend Basics",
   
